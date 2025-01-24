9ccbb67d4b816fe1e8a8b8351f6eab5aef7c547a
Anakan457 <ivancogut2017@gmail.com>
2025-01-24 21:58:20 +0200

    Add files via upload

---
+++ FirstLab/FunctionForMenu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class FunctionForMenu
+    {
+        private string _path = string.Empty;
+        public FunctionForMenu(string path = "D:\\уник\\2024 первый семестр\\Розробка вебзастосунків на базі ASP.NET\\Projects\\ConsoleApp3\\Text\\text.txt")
+        {
+            _path = path;
+        }
+
+        public int LentghTextFromFile()
+        {
+            int countWords = 0;
+            try
+            {
+                using (StreamReader reader = new StreamReader(_path))
+                {
+                   // _result = reader.ReadToEnd();
+                    string? line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        countWords += line.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+                    }
+                }
+            }catch (Exception ex)
+            {
+                Console.WriteLine("Exception - " + ex);
+            }
+           return countWords;
+        }
+
+        public double MathOperation(string operation, double x, double y)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return x + y;
+                case "-":
+                    return x - y;
+                case "*":
+                    return x * y;
+                case "/":
+                    if (y == 0) 
+                        return 0;
+                    return x / y;
+                default:
+                    Console.WriteLine("incorrect operation");
+                    return 0;
+            }
+        }
+    }
+}
---
+++ FirstLab/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionForMenu functionForMenu = new FunctionForMenu();
+            bool check = false;
+            Console.WriteLine("Welcome to my program,");
+
+            while (true)
+            {
+                Console.WriteLine("choice variant:\n1 - Lentght \"Lorem Ipsum\" text from file(you can write own file)\n" +
+                "2 - math operation(+, -, /, *)\n0 - exit");
+                if (check)
+                {
+                    Console.WriteLine("Good bye!");
+                    return;
+                }
+                string? variant  = Console.ReadLine();
+                switch (variant)
+                {
+                    case "1":
+                        int count = functionForMenu.LentghTextFromFile();
+                        Console.WriteLine(count);
+                        continue;
+                    case "2":
+                        Console.WriteLine("write a math operation\n(+, -, /, *)");
+                        string? operation = Console.ReadLine();
+                        double fisrtValue = 0;
+                        double secondValue = 0;
+                        try
+                        {
+                            Console.WriteLine("First value");
+                            fisrtValue = double.Parse(Console.ReadLine());
+                            Console.WriteLine("Second value");
+                            secondValue= double.Parse(Console.ReadLine());
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                        }
+                        double result = Math.Round(functionForMenu.MathOperation(operation, fisrtValue, secondValue), 2);
+                        Console.WriteLine(result);
+                        continue; 
+                    case "0":
+                        check = true;
+                        break;
+                    default: Console.WriteLine("Nan operation");
+                        continue;
+
+                }
+            }
+        }
+    }
+}
